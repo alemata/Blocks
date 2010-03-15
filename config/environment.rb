ee260b7d7c3ec351e0585d7135a42fca345a37c0
Alejandro Mataloni <amataloni@gmail.com>
2010-03-15 00:36:00 -0200

    Fixed will paginate gem error

--- config/environment.rb
+++ config/environment.rb
@@ -7,10 +7,12 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-
+  
   config.time_zone = 'UTC'
   config.gem "stories"
+  config.gem "will_paginate"
+
 end
 
 #require "will_paginate"
-config.load_paths += %W( #{RAILS_ROOT}/vendor/will_paginate-2.3.12/lib )
+

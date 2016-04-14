b3b121816ce39a6135aeb6683e57d4c9fde8562b
Alejandro Mataloni <amataloni@gmail.com>
2016-04-14 15:01:39 -0300

    test

--- app/models/block.rb
+++ app/models/block.rb
@@ -2,6 +2,7 @@ class Block < ActiveRecord::Base
   has_many  :revisions, :dependent => :destroy
   named_scope :visible_for_all, :conditions => {:shared => true}
   validates_presence_of :owner
+  #Testt
 
   def revisions_without_first
     self.revisions.reverse.slice(1..-1)
